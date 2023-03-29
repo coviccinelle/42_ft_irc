f4f681ab09baeb9b3263b623891de0d30e96bd85
Julien FRANCAIS <jfrancai@student.42.fr>
2023-03-29 11:58:24 +0200

    update token

--- server/Parser.cpp
+++ server/Parser.cpp
@@ -42,33 +42,36 @@ Token	Parser::_GetToken() const
 	int		state = 0;
 	string	s;
 	string::const_iterator end = _input.end();
-	while (it != end)
+	while (_it != end)
 	{
 		switch(state)
 		{
 			case 0:
 				if (_it == end) return (eoi);
-				else if (isspace(*_it)) state = 0;
-				else if (isalpha(*_it)) state = 6;
-				else if (isdigit(*_it)) state = 1;
-				else if (*_it == ':') state = 2;
-				else if (*_it == '!') state = 3;
-				else if (*_it == '@') state = 4;
+				else if (isalpha(*_it)) state = 1;
+				else if (isdit(*_it)) state = 2;
+				else if (*_it == ':') return (colon);
+				else if (*_it == SPACE) return (space);
+				else if (isspace(*_it)) state = 3;
+				else if (*_it == '!') return (exclamation_mark);
+				else if (*_it == '*') return (at);
 				++_it;
 				break ;
 			case 1:
-				if (isdigit(*_it)) state = 1;
 				break ;
 			case 2:
-				if (_it == _input.begin()) state = 5;
-				else if (_it[-1] == SPACE) state = 6;
-				++_it;
 				break ;
 			case 5:
-				// handle prefix
 				break ;
 			case 6:
-				// handle trailing
+				break ;
+			case 7:
+				break ;
+			case 8:
+				break ;
+			case 9:
+				break ;
+			case 10:
 				break ;
 		}
 	}
