68e414e2230b4a31779f4ed894dde6be9b370707
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-04-19 23:54:35 +0200

    compiled

--- server/Server.cpp
+++ server/Server.cpp
@@ -687,6 +687,7 @@ void	Server::_Join(Command &cmd, Client &client)
 	return ;
 }
 
+
 void	Server::_Part(Command &cmd, Client &client)
 {
 	cst_vec_vec_str	chanparse = _WrapChannels(cmd, 0);
@@ -715,43 +716,40 @@ void	Server::_Part(Command &cmd, Client &client)
 //            ERR_CHANOPRIVSNEEDED            ERR_NOCHANMODES
 void	Server::_Topic(Command &cmd, Client &client)
 {
-	std::list < Channel >::iterator channel;
 	cst_vec_vec_str		chans = _WrapChannels(cmd, 0);
-	cst_vec_str			targets = _WrapTargets(cmd, 0);
+	lst_chan::iterator	channel;
 
-	(void)cmd;
-	(void)client;
-	(void)channel;
-	(void)chans;
-	(void)targets;
 	std::cout << "Hey I'm command Topic: to change or view topic of chan" << std::endl;
-//	if (chans.empty())
-//		return AddData(ERR_NEEDMOREPARAMS("TOPIC"));
-//	if ((channel = _FindChannel(chans[0][chan])) == _channels.end())
-//			continue ;
-//		if (channel->_findUserIter(client.GetUinfo()[nickname]) == channel->GetUser().end())
-//			return (AddData(ERR_NOTONCHANNEL(chans[0][chan])));
-//	if (cmd.GetMiddle().size() == 1)
-//	{
-//		//Send topic of the channel back to the client
-//		if (channel->GetTopic().empty())
-//			AddData(RPL_NOTOPIC(client.GetUinfo()[nickname], client.GetUinfo()[username], client.GetUinfo()[hostname], channel->GetName()));
-//		else
-//			AddData(RPL_TOPIC(client.GetUinfo()[nickname], client.GetUinfo()[username], client.GetUinfo()[hostname], channel->GetName(), channel->GetTopic()));
-//	}
-//	else
-//	{
-//		//check if the client is an operator of the channel
-//		if (channel->_findUserIter(client.GetUinfo()[nickname])->GetMode() != '@')
+	if (chans.empty())
+		return AddData(ERR_NEEDMOREPARAMS("TOPIC"));
+	if ((channel = _FindChannel(chans[0][chan])) == _channels.end())
+		return ; //channel doesn't exist on server
+	if (channel->findUserIter(client.GetUinfo()[nickname]) == channel->GetUsers().end())
+		return (AddData(ERR_NOTONCHANNEL(chans[0][chan])));
+	if (cmd.GetCinfo()[trailing].empty())
+	{
+		//Send topic of the channel back to the client
+		if (channel->GetTopic().empty())
+			AddData(RPL_NOTOPIC(client.GetUinfo()[nickname], client.GetUinfo()[username], client.GetUinfo()[hostname], channel->GetName()));
+		else
+			AddData(RPL_TOPIC(client.GetUinfo()[nickname], client.GetUinfo()[username], client.GetUinfo()[hostname], channel->GetName(), channel->GetTopic()));
+		SendData(client.GetFd());
+	}
+	else
+	{
+		//check if the client is an operator of the channel
+		if ((channel = _FindChannel(chans[0][chan])) == _channels.end())
+			return ;
+//		if (channel->IsOperator(client) == false)
 //			return AddData(ERR_CHANOPRIVSNEEDED(chans[0][chan]));
-//		//change the topic of the channel
-//		if (cmd.GetCinfo()[trailing].empty())
-//			channel->SetTopic("");
-//		else
-//			channel->SetTopic(cmd.GetCinfo()[trailing]);
-//		//send the new topic to all the users of the channel
-//		SendChannel(channel->GetName(), "TOPIC " + channel->GetName() + " :" + channel->GetTopic() + "\r\n");
-//	}
+		//change the topic of the channel
+		if (cmd.GetCinfo()[trailing].empty())
+			channel->SetTopic("");
+		else
+			channel->SetTopic(cmd.GetCinfo()[trailing]);
+		//send the new topic to all the users of the channel
+		SendChannel(channel, channel->GetName(), "TOPIC " + channel->GetName() + " :" + channel->GetTopic() + "\r\n");
+	}
 }
 
 //Parameters: [ <channel> *( "," <channel> ) [ <target> ] ]
